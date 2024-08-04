50de9e3242309f30131f8d7be2779040f5122d44
Sabbha20 <sabbha.power1@gmail.com>
2024-08-04 21:31:26 +0530

    implemented getWalletBalance

--- index.js
+++ index.js
@@ -11,5 +11,18 @@ const wallet = new Keypair()
 const publicKey = new PublicKey(wallet._keypair.publicKey)
 const secretKey = wallet._keypair.secretKey
 
+const getWalletBalance = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`Wallet balance: ${walletBalance}`)
+    } catch (error) {
+        console.error(`Error Occurred: ${error}`)
+    }
+}
+const main = async() => {
+    await getWalletBalance()
+}
+main()
 // console.log(publicKey)
 // console.log(secretKey)
